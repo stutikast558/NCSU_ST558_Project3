2851ebfe485a934efaf52659960421b148eaa677
stutikast558 <56647064+stutikast558@users.noreply.github.com>
2019-12-13 22:20:01 -0500

    NCSU st558 Shiny App Project 3
    ui and Server files to Run the Shiny dashboard for the project

---
+++ server.R
@@ -0,0 +1,195 @@
+################################################################################################
+########  ST 558 Project 3 server code for Shiny app - NBA Player stats and Analytics ##############
+########  Student : Sridhar Tutika                                                    ############
+########  Uses package SportsAnalytics to fetch NBA player Data                       ############
+########  Also uses csv down loaded data from https://www.kaggle.com/schmadam97/nba-regular-season-stats-20182019#
+########  Produces player stats compare Barcharts that can be downloaded in csv or pdf format ##
+################################################################################################
+
+library(shiny)
+require(shinydashboard)
+library(SportsAnalytics)
+library(ggplot2)
+require(car)
+require(DT)
+require(caret)
+require(maptools)
+require(rgdal)
+require(raster)
+require(sp)
+require(rgeos)
+require(leaflet)
+require(shinyBS)
+require(RColorBrewer)
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(shinyjs)
+library(hrbrthemes)
+library(stringi)
+options(shiny.maxRequestSize=30*1024^2)
+
+shinyServer(function(input, output, session) {
+
+  ##########################
+  ## TAB CHANGE EVENTS
+  ##########################
+  observeEvent(input$tabs, {
+    ## If Player stats tab is pressed 
+    if (input$tabs == "pcompare") {
+      # --- Retrieve nba data by season based on users' input using SportsAnalytics function 
+      selectedDataAll <- reactive({
+        temp <- fetch_NBAPlayerStatistics(input$season)
+        temp[order(-temp$TotalPoints, -temp$GamesPlayed),]
+      })
+      # --- Subset data based on input (player & stats)
+      plotData <- reactive({
+        tempdf = data.frame()
+        if (input$dataType == "Total") {
+          playerA <- subset(selectedDataAll(), Name == input$PlayerA, select = c("Name", "GamesPlayed", input$stats))
+          playerB <- subset(selectedDataAll(), Name == input$PlayerB, select = c("Name", "GamesPlayed",input$stats))
+          tempdf <- rbind(playerA, playerB)
+        } 
+        else if (input$dataType == "Average") 
+        {
+          playerA <- subset(selectedDataAll(), Name == input$PlayerA, select = c("Name", "GamesPlayed",input$stats))
+          playerA[, input$stats] <- playerA[, input$stats]/playerA$GamesPlayed
+          playerB <- subset(selectedDataAll(), Name == input$PlayerB, select = c("Name", "GamesPlayed",input$stats))
+          playerB[, input$stats] <- playerB[, input$stats]/playerB$GamesPlayed
+          tempdf <- rbind(playerA, playerB)
+        }
+        
+        # reshape the date from "wide" to "long"
+        newdf <- reshape(tempdf, varying = input$stats,
+                         v.names = "NBA_Stats",
+                         timevar = "Stats_Type",
+                         times = input$stats,
+                         new.row.names = 1:10000,
+                         direction = "long")
+        newdf
+      })
+      
+      # --- Create Bar Charts
+      output$plot <- renderPlot({
+        p <- ggplot(plotData(), aes(x=Stats_Type, y=NBA_Stats, fill=Name)) +
+          geom_bar(position="dodge", stat="identity") +  coord_flip() +
+          xlab("") + 
+          ylab("") +
+          theme(axis.text=element_text(size=12),
+                axis.title=element_text(size=12,face="bold"),
+                legend.text=element_text(size=15),legend.position = "top") +
+          geom_text(aes(label=sprintf("%.02f", NBA_Stats)), 
+                    position=position_dodge(width=0.9), 
+                    vjust=-0.25, size=4)
+        
+        print(p)
+      })
+      
+      
+      # --- Download Data
+      output$downloadData <- downloadHandler(
+        filename = function() { 
+          paste(paste(input$PlayerA, input$PlayerB, sep = " VS "), 
+                ".csv", sep = "")
+        },
+        content = function(file) {
+          write.csv(plotData(), file)
+        }
+      )
+      
+      # --- Download Plot
+      output$downloadPlot <- downloadHandler(
+        filename = function() { 
+          paste(paste(input$PlayerA, input$PlayerB, sep = " VS "),
+                ".pdf", sep = "")
+        },
+        content = function(file) {
+          pdf(file)  # open the pdf device
+          thisplot <- plotData()
+          
+          p <- ggplot(thisplot, aes(x=Stats_Type, y=NBA_Stats, fill=Name)) +
+            geom_bar(position="dodge", stat="identity") +
+            geom_text(aes(label=sprintf("%.02f", NBA_Stats)), position=position_dodge(width=0.9), vjust=-0.25)
+          print(p)
+          
+          dev.off()  # turn the device off
+        }
+      )
+      ## If Team stats tab is pressed
+    } else if (input$tabs == "tstats"){
+      
+       TableData_ref <- reactive({
+         tmp_rf <- read_csv("nba_team_stats_00_to_18.csv") 
+         tmp1 <- mutate(tmp_rf,winper= paste(as.character(tmp_rf$`WIN%`*100), "%"))   
+       })
+       
+         TableData <- reactive({ 
+         tmp2 <- TableData_ref()
+         if (input$seasont == "ALL" & input$TeamA == "ALL" ){
+           df_tabledata <- tmp2
+         }else if (input$seasont == "ALL") {df_tabledata <- filter(tmp2, tmp2$TEAM == input$TeamA )
+         }else if (input$TeamA == "ALL") {df_tabledata <- filter(tmp2, tmp2$SEASON == input$seasont )
+         }else {
+           df_tabledata <- filter(tmp2, tmp2$TEAM == input$TeamA & tmp2$SEASON == input$seasont)
+         }
+         df_tabledata_2 <- dplyr::select(df_tabledata,TEAM, SEASON, 
+                                         GP, winper,PTS, `+/-`)
+         
+         attributes(df_tabledata_2)$names[1] <- "Team"
+         attributes(df_tabledata_2)$names[2] <- "Season"
+         attributes(df_tabledata_2)$names[3] <- "Games Played"
+         attributes(df_tabledata_2)$names[4] <- "Win %"
+         attributes(df_tabledata_2)$names[5] <- "Points"
+         attributes(df_tabledata_2)$names[6] <- "Player/Team Impact"
+         df_tabledata_2
+      })
+         ## Send the table based on selection
+         output$table <- renderDT(TableData())
+         
+         ## Plot 2 data
+         # 
+          output$plot2 <- renderPlot({
+         #   get the data
+            tmp_plot1 <- TableData_ref() 
+            tmp_plot2 <- filter(tmp_plot1, TEAM == input$TeamA)
+         #   
+         #   #create a scatter plot, set it to all seasons
+            
+            observe({updateSelectInput(session, "seasont", selected = "ALL")})
+             
+             p2 <- ggplot(tmp_plot2, aes(x = stri_sub(SEASON,-5,-1), y = `WIN%`*100))+
+                   geom_line(color="#69b3a2") +
+                    geom_point(color="#69b3a2", size=4) +
+               labs(x = "Season", y = "Win %", title(paste(tmp_plot2$TEAM, " ", "Team Win % Visual")))
+             print(p2)
+            #theme_ipsum() 
+          })
+      
+    }
+    
+  })
+  
+#################################################################################################
+#### Defautls to Overview Tab ###################################################################
+######################################################################################   
+    output$instructions <- renderText({
+              includeHTML("rmdhtml/help1r.rhtml")
+            })
+    observeEvent(input$help1, {
+      output$instructions <- renderText({
+        includeHTML("rmdhtml/help1r.rhtml")
+      })
+    })
+    observeEvent(input$help2, {
+      output$instructions <- renderText({
+        includeHTML("rmdhtml/Supervised_model.html")
+      })
+    })
+    observeEvent(input$help3, {
+      output$instructions <- renderText({
+        includeHTML("rmdhtml/UnSupervised_model_PCA.html")
+      })
+    })
+})
+
+###################### END of Server code   ######################
---
+++ ui.R
@@ -0,0 +1,214 @@
+################################################################################################
+########  ST 558 Project 3 ui code for Shiny app - NBA Player stats and Analytics ##############
+########  Student : Sridhar Tutika                                                ##############
+########  Uses package SportsAnalytics to fetch NBA player Data                   ##############
+########  Also uses csv down loaded data from https://www.kaggle.com/schmadam97/nba-regular-season-stats-20182019#
+################################################################################################
+require(shiny)
+require(shinydashboard)
+require(leaflet)
+require(shinyBS)
+library(SportsAnalytics)
+library(shinyjs)
+library(readr)
+library(DT)
+
+header <- dashboardHeader(title = "NBA Stas Dashboard ")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    id = "tabs",
+    menuItem(
+      "Overview", icon = icon("info", lib = "font-awesome"), tabName = "overview"
+    ),
+    menuItem(
+      "Player Stats Compare", icon = icon("user-friends", lib = "font-awesome"), tabName = "pcompare"
+    ),
+    menuItem(
+      "Team Stats", icon = icon("basketball-ball", lib = "font-awesome"), tabName = "tstats"
+    )
+  #  menuItem(
+  #    "Supervised Model", icon = icon("flash", lib = "font-awesome"), tabName = "smodel"
+  #  ),
+  #  menuItem(
+  #    "Unsupervised Model", icon = icon("magic", lib = "font-awesome"), tabName = "umodel"
+  #  )
+  )
+)
+########## function from SportsAnalytics to get player data
+selectedDataAll1 <- function(){
+  temp1 <- fetch_NBAPlayerStatistics(season = "18-19")
+  temp1[order(-temp1$TotalPoints, -temp1$GamesPlayed),]
+}
+########## Reading csv downloaded data from 
+######## https://www.kaggle.com/schmadam97/nba-regular-season-stats-20182019#
+######## for team stats
+nba_teams <- function(){
+  tmp2 <- read_csv("nba_team_stats_00_to_18.csv")
+}
+
+body <- dashboardBody(
+                  tags$head(tags$style(
+                  HTML('.skin-blue .wrapper{background-color:#ecf0f5}')
+                  )),
+        ## code for player stats menu          
+        tabItems(
+          tabItem(
+          tabName = "pcompare",
+          fluidRow(
+           column(
+                width = 4,
+                 box(  
+              title = "Player stats", status = "danger", width = 14, solidHeader = TRUE,
+              selectInput("season","NBA Season",
+            # select Season
+                  c(
+                        "01-02" = "01-02",
+                        "02-03" = "02-03",
+                        "03-04" = "03-04",
+                        "04-05" = "04-05",
+                        "05-06" = "05-06",
+                        "06-07" = "06-07",
+                        "07-08" = "07-08",
+                        "08-09" = "08-09",
+                        "09-10" = "09-10",
+                        "10-11" = "10-11",
+                        "11-12" = "11-12",
+                        "12-13" = "12-13",
+                        "13-14" = "13-14",
+                        "14-15" = "14-15",
+                        "15-16" = "15-16",
+                        "16-17" = "16-17",
+                        "17-18" = "17-18",
+                        "18-19" = "18-19"
+                                        ),
+                        selected = "18-19"
+                        ),
+                          selectInput("PlayerA", label = h4("Select Player A"),
+                            choices = as.list(as.character(unique(selectedDataAll1()$Name))),
+                        #    select 1st player
+                            selected = as.list(as.character(unique(selectedDataAll1()$Name)))[1],
+                            selectize = TRUE,
+                            multiple = FALSE),
+                          selectInput("PlayerB", label = h4("Select Player B"),
+                            choices = as.list(as.character(unique(selectedDataAll1()$Name))),
+                        # # select 2nd player
+                            selected = as.list(as.character(unique(selectedDataAll1()$Name)))[2],
+                            selectize = TRUE,
+                            multiple = FALSE),
+        
+                          radioButtons(
+                            "dataType",
+                            label = "Stat Type",
+                            choices = c("Average", "Total"),
+                            selected = "Average"
+                          ),
+          # select data type
+                              checkboxGroupInput(
+                                "stats",
+                                label = "Stats to compare",
+                                choices = c(
+                                  "Points" = "TotalPoints",
+                                  "Threes Made" = "ThreesMade",
+                                  "Rebounds" = "TotalRebounds",
+                                  "Assists" = "Assists",
+                                  "Steals" = "Steals",
+                                  "Blocks" = "Blocks",
+                                  "Turnovers" = "Turnovers"
+                                ),
+                                selected = c(
+                                  "TotalPoints",
+                                  "ThreesMade",
+                                  "TotalRebounds",
+                                  "Assists",
+                                  "Steals",
+                                  "Blocks",
+                                  "Turnovers"
+                                ),
+                                inline = "TRUE"
+                              ),
+                              # select data type
+                              
+                              downloadButton('downloadData', 'Download Data'),
+                              
+                              downloadButton('downloadPlot', 'Download Plot')
+                               ),
+          ),
+       box(width = 8,  plotOutput("plot", height=500))
+      )),
+    #####code for Team stats menu
+    tabItem(
+      tabName = "tstats",
+      #bsAlert("alert0"),
+      fluidRow(
+        column(
+          width = 3,
+          box(  
+            title = "Team stats 2018-19", status = "primary", width = 14, solidHeader = TRUE,
+            selectInput("seasont","NBA Season",
+                        # select Season
+                        c(
+                          "2001-02" = "2001-02",
+                          "2002-03" = "2002-03",
+                          "2003-04" = "2003-04",
+                          "2004-05" = "2004-05",
+                          "2005-06" = "2005-06",
+                          "2006-07" = "2006-07",
+                          "2007-08" = "2007-08",
+                          "2008-09" = "2008-09",
+                          "2009-10" = "2009-10",
+                          "2010-11" = "2010-11",
+                          "2011-12" = "2011-12",
+                          "2012-13" = "2012-13",
+                          "2013-14" = "2013-14",
+                          "2014-15" = "2014-15",
+                          "2015-16" = "2015-16",
+                          "2016-17" = "2016-17",
+                          "2017-18" = "2017-18",
+                          "2018-19" = "2018-19",
+                          "ALL" = "ALL"
+                        ),
+                        selected = "ALL"
+            ),
+            selectInput("TeamA", label = h4("Select a Team"),
+                        choices = as.list(as.character(unique(nba_teams()$TEAM))),
+                        selected = as.list(as.character(unique(nba_teams()$TEAM[1]))),
+                        selectize = TRUE,
+                        multiple = FALSE),
+            # radioButtons(
+            #   "tviewType",
+            #   label = "Tables or Plots",
+            #   choices = c("Table", "Plots"),
+            #   selected = "Table"
+            # ),
+            ),
+        ),
+        tabBox(
+          side = "left", width=9, height = "500",
+          selected = "Team_Table",
+          tabPanel("Team_Table",  DTOutput("table")),
+          tabPanel("Team_Performance",plotOutput("plot2")))
+        ##### this is working below
+            #  box(width = 8,  DTOutput("table"))
+        ####### this was working above
+      )),
+    ### Code for Introduction and Information Menu
+    tabItem(tabName = "overview",
+            fluidRow(column(
+              4,
+              box(
+                title = "Contents", status = "primary", width = 12, solidHeader = TRUE,
+                div(actionLink("help1", "Introduction"), style = "font-size: 125%"),
+                div(actionLink("help2", "Supervised Learning Models"), style = "font-size: 125%"),
+                div(actionLink("help3", "Principal Components analysis"), style = "font-size: 125%")
+              )
+            )
+            ,
+            column(
+              8, box(htmlOutput('instructions'),status = "primary", width = 12, solidHeader = TRUE)
+            )))
+))
+ui <- dashboardPage(skin="red", header,sidebar, body)
+##############################################################
+############  End of UI Code  ################################
+##############################################################
